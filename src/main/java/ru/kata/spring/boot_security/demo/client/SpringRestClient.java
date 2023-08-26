4aa379e0f9709b8445b6a75ad9ff7b7efa409c84
YuriSolom <ziemssenjoach@gmail.com>
2023-08-26 23:47:22 +0300

    Task's completed!

--- src/main/java/ru/kata/spring/boot_security/demo/client/SpringRestClient.java
+++ src/main/java/ru/kata/spring/boot_security/demo/client/SpringRestClient.java
@@ -5,18 +5,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.kata.spring.boot_security.demo.entity.Employee;
 
-import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Component
+
 public class SpringRestClient {
-    private static final String GET_EMPLOYEES_ENDPOINT_URL = "http://94.198.50.185:7081/api/users";
-    private static final String CREATE_EMPLOYEE_ENDPOINT_URL = "http://94.198.50.185:7081/api/users";
-    private static final String UPDATE_EMPLOYEE_ENDPOINT_URL = "http://94.198.50.185:7081/api/users";
-    private static final String DELETE_EMPLOYEE_ENDPOINT_URL = "http://94.198.50.185:7081/api/users/{id}";
+    private static final String URL = "http://94.198.50.185:7081/api/users";
+    private static final String DELETE_URL = "http://94.198.50.185:7081/api/users/{id}";
     private static RestTemplate restTemplate = new RestTemplate();
     String cookies;
 
@@ -33,7 +30,7 @@ public class SpringRestClient {
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
-        ResponseEntity<String> result = restTemplate.exchange(GET_EMPLOYEES_ENDPOINT_URL, HttpMethod.GET, entity,
+        ResponseEntity<String> result = restTemplate.exchange(URL, HttpMethod.GET, entity,
                 String.class);
         cookies = result.getHeaders().get("Set-Cookie").stream().collect(Collectors.joining(";"));
         System.out.println(result);
@@ -44,10 +41,10 @@ public class SpringRestClient {
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.set("Cookie", cookies);
-            Employee newEmployee = new Employee("James", "Brown", 33);
+            Employee newEmployee = new Employee(3,"James", "Brown", 33);
             HttpEntity<Employee> entity = new HttpEntity<>(newEmployee, headers);
             RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.exchange(CREATE_EMPLOYEE_ENDPOINT_URL, HttpMethod.POST, entity, String.class);
+            ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.POST, entity, String.class);
             System.out.println(response.getBody());
         } catch (Throwable e) {
             e.printStackTrace();
@@ -61,7 +58,7 @@ public class SpringRestClient {
             Employee updatedEmployee = new Employee(3, "Thomas", "Shelby", 33);
             HttpEntity<Employee> entity = new HttpEntity<>(updatedEmployee, headers);
             RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.exchange(UPDATE_EMPLOYEE_ENDPOINT_URL, HttpMethod.PUT, entity, String.class);
+            ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.PUT, entity, String.class);
             System.out.println(response.getBody());
         } catch (Throwable e) {
             e.printStackTrace();
@@ -76,7 +73,7 @@ public class SpringRestClient {
             Map<String, String> params = new HashMap<>();
             params.put("id", "3");
             RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.exchange(DELETE_EMPLOYEE_ENDPOINT_URL, HttpMethod.DELETE, entity, String.class, params);
+           ResponseEntity<String> response = restTemplate.exchange(DELETE_URL, HttpMethod.DELETE, entity, String.class, params);
             System.out.println(response.getBody());
         } catch (Throwable e) {
             e.printStackTrace();
--- src/main/java/ru/kata/spring/boot_security/demo/entity/Employee.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Employee.java
@@ -21,12 +21,6 @@ public class Employee {
     public Employee() {
     }
 
-    public Employee(String name, String lastName, int age) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
     public Employee(int id, String name, String lastName, int age) {
         this.id = id;
         this.name = name;
